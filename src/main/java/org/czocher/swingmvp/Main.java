baf42c2502e2cc9b6d61d6ca34f33ea0870c3e39
Paweł Jan Czochański <czochanski@gmail.com>
2015-11-18 23:08:36 +0100

    Removed unused JFrame object

--- src/main/java/org/czocher/swingmvp/Main.java
+++ src/main/java/org/czocher/swingmvp/Main.java
@@ -1,17 +1,13 @@
 package org.czocher.swingmvp;
 
 import com.google.common.eventbus.EventBus;
-import javax.swing.JFrame;
 
 public class Main {
     
     public static void main(String[] args) {
         EventBus eventBus = EventBusSingleton.getInstance();
         
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         MainAppPresenter presenter = new MainAppPresenter(eventBus);
-        presenter.go(frame);
+        presenter.go(null);
     }
 }
